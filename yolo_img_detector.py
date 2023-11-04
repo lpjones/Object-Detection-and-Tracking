9c0c1df904837c8e11217d0da96bd7358e6a4f48
lpjones <lpjones@ucdavis.edu>
2023-11-04 15:07:50 -0700

    Only need to add flag functionality

--- yolo_img_detector.py
+++ yolo_img_detector.py
@@ -57,32 +57,37 @@ parser.add_argument('-classes_im', action = "store_true", help = "print classes 
 
 args = parser.parse_args()
 
-# Check for correct num of arguments
-if len(args.input_files) < 4:
-    print("Invalid number of input files. Need at least 4\n \
-            <Weights file>   <config file>   <labels file>  <Image/Images path>")
-    exit(0)
 
-imgs = []
-# Check if input image path is a directory
-if os.path.isdir(args.input_files[3]):
-    # If it is a directory pull out images from directory
-    for file in os.listdir(args.input_files[3]):
-        if file.endswith('.jpg'):
-            imgs.append(args.input_files[3] + file)
-else:
-    # Check for valid file paths
-    for file in args.input_files:
-        if not os.path.isfile(file):
-            print(f"{file} is not a valid file path")
-            exit(0)
-    imgs = args.input_files[3:]
 
-try:
-    labels = pd.read_csv(args.input_files[2])
-except:
-    print(f"Invalid Labels file {args.input_files[2]}")
-    exit(0)
+def check_input_paths(args):
+    imgs = []
+
+    # Check for correct num of arguments
+    if len(args.input_files) < 4:
+        print("Invalid number of input files. Need at least 4\n \
+                <Weights file>   <config file>   <labels file>  <Image/Images path>")
+        exit(0)
+
+    # Check if input image path is a directory
+    if os.path.isdir(args.input_files[3]):
+        # If it is a directory pull out images from directory
+        for file in os.listdir(args.input_files[3]):
+            if file.endswith('.jpg'):
+                imgs.append(args.input_files[3] + file)
+    else:
+        # Check for valid file paths
+        for file in args.input_files:
+            if not os.path.isfile(file):
+                print(f"{file} is not a valid file path")
+                exit(0)
+        imgs = args.input_files[3:]
+
+    try:
+        labels = pd.read_csv(args.input_files[2], header=None)
+    except:
+        print(f"Invalid Labels file {args.input_files[2]}")
+        exit(0)
+    return imgs, labels
 
 
 if args.classes_im:
@@ -99,33 +104,113 @@ def load_YOLO(model_architecture, model_weights):
     except:
         print("Invalid Weights or Config file")
         exit(0)
-    layers = network.getLayerNames()
     yolo_layers = ['yolo_82', 'yolo_94', 'yolo_106']
+    return yolo_layers, network
 
 
 # Check if image file is valid
 def read_img(image_path_arr):
     fig, ax = plt.subplots(1, len(image_path_arr), figsize=(20, 20), squeeze=False)
-    
+    images_cv2 = []
     for i, image_path in enumerate(image_path_arr):
         try:
             image = cv2.imread(image_path)
+            images_cv2.append(image)
             ax[0][i].axis('off')
             ax[0][i].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         except:
             print(f"Invalid Input Image {image_path}")
             exit(0)
-        
-    plt.show()
-
-load_YOLO(args.input_files[1], args.input_files[0])
-
-read_img(imgs)
-
-# Convert Image to Blob
+    
+    #plt.show()
+    return images_cv2
 
 
-# Pass image through the network
+
+# Convert Images to Blobs
+def im2Blob(images_cv2):
+    imgs_blob = []
+    for im in images_cv2:
+        input_blob = cv2.dnn.blobFromImage(im, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        imgs_blob.append(input_blob)
+    return imgs_blob
 
 
-# Define Variables for drawing on image
+# Pass images through the network
+def imgs_to_network(blob_imgs, yolo_layers, network):
+    outputs = []
+    for im in blob_imgs:
+        network.setInput(im)
+        output = network.forward(yolo_layers)
+        outputs.append(output)
+    return outputs
+
+
+
+def draw_image(image, output, labels):
+    # Define variables for drawing on image
+    bounding_boxes = []
+    confidences = []
+    classes = []
+    probability_minimum = 0.5
+    threshold = 0.3
+    h, w = image.shape[:2]
+    # Get bounding boxes, confidences and classes
+    for result in output:
+        for detection in result:
+            scores = detection[5:]
+            class_current = np.argmax(scores)
+            confidence_current = scores[class_current]
+            if confidence_current > probability_minimum:
+                box_current = detection[0:4] * np.array([w, h, w, h])
+                x_center, y_center, box_width, box_height = box_current.astype('int')
+                x_min = int(x_center - (box_width / 2))
+                y_min = int(y_center - (box_height / 2))
+                bounding_boxes.append([x_min, y_min, int(box_width), int(box_height)])
+                confidences.append(float(confidence_current))
+                classes.append(class_current)
+
+    # Draw bounding boxes and information on images
+    results = cv2.dnn.NMSBoxes(bounding_boxes, confidences, probability_minimum, threshold)
+    coco_labels = 80
+    np.random.seed(42)
+    colours = np.random.randint(0, 255, size=(coco_labels, 3), dtype='uint8')
+
+    if len(results) > 0:
+        for i in results.flatten():
+            x_min, y_min = bounding_boxes[i][0], bounding_boxes[i][1]
+            box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
+            colour_box = [int(j) for j in colours[classes[i]]]
+            cv2.rectangle(image, (x_min, y_min), (x_min + box_width, y_min + box_height),
+                        colour_box, 5)
+            text_box = labels[0][classes[i]] + ': {:.2f}'.format(confidences[i])
+            cv2.putText(image, text_box, (x_min, y_min - 7), cv2.FONT_HERSHEY_SIMPLEX, .75, colour_box, 2)
+    return image
+
+
+imgs, labels = check_input_paths(args)
+
+yolo_layers, network = load_YOLO(args.input_files[1], args.input_files[0])
+
+imgs_cv2 = read_img(imgs)
+
+imgs_blob = im2Blob(imgs_cv2)
+
+outputs = imgs_to_network(imgs_blob, yolo_layers, network)
+
+output_path = './out_images/'
+
+# Match name to output label number
+
+try:
+    os.mkdir(output_path)
+except:
+    pass
+# Output images with boxes as files in out_images
+for i, im in enumerate(imgs_cv2):
+    img = draw_image(im, outputs[i], labels)
+    # Extract file name with extension
+    file = str(os.path.basename(imgs[i]))
+    # Remove extension
+    file = str(os.path.splitext(file)[0])
+    plt.imsave(output_path + file + '_out.jpg', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
