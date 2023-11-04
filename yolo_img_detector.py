5e73fe883fc7420577484cb1b5ee59c0f0d45bf5
Luke Jones <lukejones@Lukes-MacBook-Pro-5.local>
2023-11-03 22:37:10 -0700

    Command Line Argument Parsing Complete

--- yolo_img_detector.py
+++ yolo_img_detector.py
@@ -1,7 +1,125 @@
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import pandas as pd
 
-# TODO: Phase 1
+"""
+    1) Parse arguments passed to program (path to images, coco names (class) file,
+       different types of outputs (classifications, how many of each classification))
+
+    2) Load yolo model, convert image to blob
+
+    3) Pass images into yolo model to make inferences
+
+    4) Take output of yolo model and match classification number to corresponding name in coconames
+        Draw the bounding boxes on the images using the regression output
+        Save bounded box image with label names to output file in out_imgs
+    
+    Things to track:
+        Total number of classes detected
+        Average inference time
+        How many of each class total
+        How many of each class per image
+"""
+
+"""
+    python3 yolo_img_detector  <Weights file>   <config file>   <labels file>  <Image/Images path> \
+                                -inf  -classes_im 
+
+    Default:
+        -inf
+        -classes_all
+    
+    -h = This help menu
+    
+    -classes_im = Per Image Breakdown
+        Per Image Breakdown
+        sample.jpg => Car: 1
+        
+    -inf = Show average inference time
+        Average Inference Time: 0.613 seconds
+
+    -classes_all = show all classes detected
+"""
+
+# Parse in arguments from command line
+# Referenced from argparse library
+parser = argparse.ArgumentParser(description="Command-Line Parser")
+
+# Define a positional argument, our input_file
+parser.add_argument('input_files', nargs='+', type=str, help='<Weights file>   <config file>   <labels file>  <Image/Images path>')
+parser.add_argument('-inf', action = "store_true", help="Display average inference time")
+parser.add_argument('-classes_all', action = "store_true", help="Display all classes detected")
+parser.add_argument('-classes_im', action = "store_true", help = "Classes per image, per image breakdown")
 
 
+args = parser.parse_args()
+
+# Check for correct num of arguments
+if len(args.input_files) < 4:
+    print("Invalid number of input files. Need at least 4\n \
+            <Weights file>   <config file>   <labels file>  <Image/Images path>")
+    exit(0)
+
+# Check for valid file paths
+for file in args.input_files:
+    if not os.path.isfile(file):
+        print(f"{file} is not a valid file path")
+        exit(0)
+
+try:
+    labels = pd.read_csv(args.input_files[2])
+except:
+    print(f"Invalid Labels file {args.input_files[2]}")
+    exit(0)
+
+
+print(args.input_files)
+if args.classes_im:
+    print("classes_im is selected")
+if args.classes_all:
+    print("classes_all is selected")
+if args.inf: # True value is stored in args.inf
+    print("-inf is selected")
+
+print(cv2.__version__)
+
+# Load YOLO model
+def load_YOLO(model_architecture, model_weights):
+    try:
+        network = cv2.dnn.readNetFromDarknet(model_architecture, model_weights)
+    except:
+        print("Invalid Weights or Config file")
+        exit(0)
+    layers = network.getLayerNames()
+    yolo_layers = ['yolo_82', 'yolo_94', 'yolo_106']
+
+load_YOLO(args.input_files[1], args.input_files[0])
+
+print(args.input_files[3:])
+
+# Check if image file is valid
+def read_img(image_path_arr):
+    fig, ax = plt.subplots(1, len(image_path_arr), figsize=(20, 20))
+    for i, image_path in enumerate(image_path_arr):
+        try:
+            image = cv2.imread(image_path)
+            ax[i].axis('off')
+            ax[i].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        except:
+            print(f"Invalid Input Image {image_path}")
+            exit(0)
+        
+    plt.show()
+
+read_img(args.input_files[3:])
+
+# Convert Image to Blob
+
+
+# Pass image through the network
+
+
+# Define Variables for drawing on image
