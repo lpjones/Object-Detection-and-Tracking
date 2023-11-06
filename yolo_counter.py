f649532ae615fd0b6c8f15cc78b04cb78dd32453
lpjones <lpjones@ucdavis.edu>
2023-11-05 17:42:45 -0800

    Finished Phase 1 Started Phase 2

--- yolo_counter.py
+++ yolo_counter.py
@@ -1,6 +1,172 @@
 import os
 import cv2
 from sort import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# TODO: Phase 2
+"""
+    1. Parse input files <YOLO weights file> <YOLO config file> <Label file> <Path to input video>
+    
+    2. Modify code so we only draw/annotate boxes for people
 
+    3. Use MOTs (SORT tracker) to assign ID's to bound boxes, refer to sort.md and sort.py 
+
+    4. Save new video with bounding boxes and counter for number of people to "mot_vid/MOTS20-09-result.mp4"
+    
+"""
+
+# Parse in arguments from command line
+# Referenced from argparse library
+parser = argparse.ArgumentParser(description="Command-Line Parser")
+
+# Define a positional argument, our input_file
+parser.add_argument('input_files', nargs='+', type=str, help='<Weights file>   <config file>   <labels file>  <Images/Image dir>')
+parser.add_argument('-inf', action = "store_true", help="print average inference time")
+parser.add_argument('-classes_all', action = "store_true", help="print all classes detected")
+parser.add_argument('-classes_im', action = "store_true", help = "print classes per image")
+
+
+args = parser.parse_args()
+
+
+
+def check_input_paths(args):
+    # Check for correct num of arguments
+    if len(args.input_files) != 4:
+        print("Invalid number of input files. Need at 4\n \
+                <Weights file>   <config file>   <labels file>  <Video Path>")
+        exit(0)
+
+    # Check for valid file paths
+    if not os.path.isfile(args.input_files[3]):
+        print(f"{args.input_files[3]} is not a valid file path")
+        exit(0)
+
+    video = args.input_files[3]
+
+    try:
+        labels = pd.read_csv(args.input_files[2], header=None)
+    except:
+        print(f"Invalid Labels file {args.input_files[2]}")
+        exit(0)
+    return video, labels
+
+# Load YOLO model
+def load_YOLO(model_architecture, model_weights):
+    try:
+        network = cv2.dnn.readNetFromDarknet(model_architecture, model_weights)
+    except:
+        print("Invalid Weights or Config file")
+        exit(0)
+    yolo_layers = ['yolo_82', 'yolo_94', 'yolo_106']
+    return yolo_layers, network
+
+
+# Return array of all frames in video
+def read_video(video_path):
+    frames = []
+    video = cv2.VideoCapture(video_path)
+    if video.isOpened() == False:
+            print(f"Invalid Video {video_path}")
+            exit(0)
+
+    while(video.isOpened()):
+        ret, frame = video.read()
+        
+        if not ret: # TODO: See how to implement an end of video
+            print("End of video")
+            break
+        
+        frames.append(frame) # Store frame into array
+        
+        #cv2.imshow('Frame', frame)
+        #if cv2.waitKey(25) == ord('q'):
+        #    break
+        
+    return frames
+
+
+# Convert Images to Blobs
+def im2Blob(im):
+    input_blob = cv2.dnn.blobFromImage(im, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    return [input_blob] # Add dimension so that output is (1, 3, 416, 416)
+
+
+# Pass images through the network
+def imgs_to_network(blob_imgs, yolo_layers, network):
+    outputs = []
+    times = []
+    for im in blob_imgs:
+        print(np.array(im).shape)
+        network.setInput(im)
+        start_time = time.time()
+        output = network.forward(yolo_layers)
+        end_time = time.time()
+        times.append(end_time - start_time)
+        outputs.append(output)
+    return outputs, sum(times) / len(times)
+
+
+
+def draw_image(image, output, labels):
+    # Define variables for drawing on image
+    bounding_boxes = []
+    confidences = []
+    classes = []
+    probability_minimum = 0.5
+    threshold = 0.3
+    h, w = image.shape[:2]
+    # Get bounding boxes, confidences and classes
+    for result in output:
+        for detection in result:
+            scores = detection[5:]
+            class_current = np.argmax(scores)
+            confidence_current = scores[class_current]
+            if confidence_current > probability_minimum:
+                box_current = detection[0:4] * np.array([w, h, w, h])
+                x_center, y_center, box_width, box_height = box_current.astype('int')
+                x_min = int(x_center - (box_width / 2))
+                y_min = int(y_center - (box_height / 2))
+                bounding_boxes.append([x_min, y_min, int(box_width), int(box_height)])
+                confidences.append(float(confidence_current))
+                classes.append(class_current)
+
+    # Draw bounding boxes and information on images
+    results = cv2.dnn.NMSBoxes(bounding_boxes, confidences, probability_minimum, threshold)
+    coco_labels = 80
+    np.random.seed(42)
+    colours = np.random.randint(0, 255, size=(coco_labels, 3), dtype='uint8')
+    # Track number of each class predicted per image
+    if len(results) > 0:
+        for i in results.flatten(): # i is the value, , not the index of the results array
+            if labels[0][classes[i]] == 'person':
+                x_min, y_min = bounding_boxes[i][0], bounding_boxes[i][1]
+                box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
+                colour_box = [int(j) for j in colours[classes[i]]]
+                cv2.rectangle(image, (x_min, y_min), (x_min + box_width, y_min + box_height), colour_box, 5)
+                text_box = labels[0][classes[i]] + ': {:.2f}'.format(confidences[i])
+                cv2.putText(image, text_box, (x_min, y_min - 7), cv2.FONT_HERSHEY_SIMPLEX, .75, colour_box, 2)
+    return image
+
+
+video, labels = check_input_paths(args)
+
+yolo_layers, network = load_YOLO(args.input_files[1], args.input_files[0])
+
+video_frames = read_video(video)
+
+print(np.array(video_frames[0]).shape)
+
+for i, frames in enumerate(video_frames): # Loop through all frames in the video
+
+    imgs_blob = im2Blob(frames)
+
+    outputs, avg_time = imgs_to_network(imgs_blob, yolo_layers, network) # Inferences are the outputs
+
+    box_tracker = Sort()
+
+    a = box_tracker.update(outputs[i])
+
+    print(a)
+    boxed_frame = draw_image(frames, outputs[i], labels)
--- yolo_img_detector.py
+++ yolo_img_detector.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import pandas as pd
+import time
 
 """
     1) Parse arguments passed to program (path to images, coco names (class) file,
@@ -89,14 +90,6 @@ def check_input_paths(args):
         exit(0)
     return imgs, labels
 
-
-if args.classes_im:
-    print("classes_im is selected")
-if args.classes_all:
-    print("classes_all is selected")
-if args.inf: # True value is stored in args.inf
-    print("-inf is selected")
-
 # Load YOLO model
 def load_YOLO(model_architecture, model_weights):
     try:
@@ -139,11 +132,16 @@ def im2Blob(images_cv2):
 # Pass images through the network
 def imgs_to_network(blob_imgs, yolo_layers, network):
     outputs = []
+    times = []
     for im in blob_imgs:
+        print(np.array(im).shape)
         network.setInput(im)
+        start_time = time.time()
         output = network.forward(yolo_layers)
+        end_time = time.time()
+        times.append(end_time - start_time)
         outputs.append(output)
-    return outputs
+    return outputs, sum(times) / len(times)
 
 
 
@@ -175,17 +173,22 @@ def draw_image(image, output, labels):
     coco_labels = 80
     np.random.seed(42)
     colours = np.random.randint(0, 255, size=(coco_labels, 3), dtype='uint8')
-
+    # Track number of each class predicted per image
+    classes_dict = {}
     if len(results) > 0:
-        for i in results.flatten():
+        for i in results.flatten(): # i is the value, , not the index of the results array
             x_min, y_min = bounding_boxes[i][0], bounding_boxes[i][1]
             box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
             colour_box = [int(j) for j in colours[classes[i]]]
-            cv2.rectangle(image, (x_min, y_min), (x_min + box_width, y_min + box_height),
+            cv2.rectangle(image, (x_min, y_min), (x_min + box_width, y_min +    box_height),
                         colour_box, 5)
             text_box = labels[0][classes[i]] + ': {:.2f}'.format(confidences[i])
             cv2.putText(image, text_box, (x_min, y_min - 7), cv2.FONT_HERSHEY_SIMPLEX, .75, colour_box, 2)
-    return image
+            if labels[0][classes[i]] not in classes_dict:
+                classes_dict[labels[0][classes[i]]] = 1 # If "person" not in dictionary, add it to dict, it starts w/ one instance
+            else:
+                classes_dict[labels[0][classes[i]]] += 1 # Get the current num of instances of "person," and increase it by 1, if you see in array [1, 2, 4, 1] in results, there are two persons in the  image
+    return image, classes_dict
 
 
 imgs, labels = check_input_paths(args)
@@ -194,23 +197,52 @@ yolo_layers, network = load_YOLO(args.input_files[1], args.input_files[0])
 
 imgs_cv2 = read_img(imgs)
 
+print(np.array(imgs_cv2[0]).shape)
+
 imgs_blob = im2Blob(imgs_cv2)
 
-outputs = imgs_to_network(imgs_blob, yolo_layers, network)
+outputs, avg_time = imgs_to_network(imgs_blob, yolo_layers, network) # Inferences are the outputs
 
-output_path = './out_images/'
+output_path = './out_imgs/'
 
-# Match name to output label number
-
+# Create out_images/ if it doesn't exist
 try:
     os.mkdir(output_path)
 except:
     pass
-# Output images with boxes as files in out_images
+dicts, img_names = [], []
+classes_all_dict = {} # Shows total class instances across all images
+# save images with boxes as files in out_images/
 for i, im in enumerate(imgs_cv2):
-    img = draw_image(im, outputs[i], labels)
+    img, class_dict = draw_image(im, outputs[i], labels) # Draw inferences made in imgs_to_network() to the original images (imgs_cv2)
+    dicts.append(class_dict)
     # Extract file name with extension
     file = str(os.path.basename(imgs[i]))
+    img_names.append(file)
     # Remove extension
     file = str(os.path.splitext(file)[0])
+    # Save to file
     plt.imsave(output_path + file + '_out.jpg', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+if args.classes_im: # If flag -classes_im is true
+    print('Per Image Breakdown')
+    for i, im in enumerate(dicts):
+        print(img_names[i]) # Print current image name
+        for cl in im:
+            print(cl + ':', im[cl]) # Print out class and value of that class found in current image
+        print('\n', end='')
+if args.classes_all: # If flag -classes_all is true
+    print('Total Detection Breakdown') # Across all the images you scanned, how many of each class did you see in total?
+    # Go through dict of each image and add to total
+    for im in dicts: # dicts is array of dicts, each image has its own dict
+        for cl in im: # For classes encountered in current image dict
+            if cl not in classes_all_dict: 
+                classes_all_dict[cl] = im[cl] # For the curr image you are on, add the curr class you are on (that has not yet been encountered)
+            else:
+                classes_all_dict[cl] += im[cl] # For the curr i
+    # Print out classes with how many of each
+    for cl in classes_all_dict:
+        print(cl + ':', classes_all_dict[cl])
+    print('\nTotal Number of Classes Detected:', len(classes_all_dict))
+if args.inf: # True value is stored in args.inf
+    print(f"Average Inference Time: %.3f"% avg_time)
